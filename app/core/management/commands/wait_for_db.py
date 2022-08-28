caabeb95f8f6b7faa92165d2e1db034cb4f16dd6
rajan baliyan <rajansinghbaliyan@gmail.com>
2022-08-28 07:44:27 +0530

    feat: added the basic stub for custom command creation

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,9 @@
+"""Django command to wait until the database is available."""
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        pass
+   
