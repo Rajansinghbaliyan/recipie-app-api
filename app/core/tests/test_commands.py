b8eb7129fca21e100176a802ce8da66bc3b779bb
rajan baliyan <rajansinghbaliyan@gmail.com>
2022-08-28 08:35:47 +0530

    fix: typo

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -14,7 +14,7 @@ class CommandsTests(SimpleTestCase):
 
         call_command("wait_for_db")
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, _, patched_check):
@@ -25,4 +25,4 @@ class CommandsTests(SimpleTestCase):
 
         self.assertEqual(patched_check.call_count, 6)
 
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
